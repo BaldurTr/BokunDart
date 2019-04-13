7e6106836c73d6966f1ff42ca61fbf9154a493bc
Baldur Tryggvason <ballit85@gmail.com>
2019-04-13 19:07:03 +0000

    Successfully deserialize Snapshot from Firebase

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -7,60 +7,66 @@ class User {
       this._gameHistory, this._highScores, this._monthlyElo);
 
   User.fromSnapshot(DataSnapshot snapshot) {
-    _userName = snapshot.value['username'];
-    _email = snapshot.value['email'];
-    _photoUrl = snapshot.value['photoURL'];
-    _gameHistory = createMatchList(snapshot.value['scoreHistory']);
-    _monthlyElo = createMonthlyMatchMap(snapshot.value['monthlyElo']);
-    _highScores = createHighScoreList(snapshot.value['highestScore']);
+    final snap_data = Map<String, dynamic>.from(snapshot.value);
+
+    _userName = snap_data['username'];
+    _email = snap_data['email'];
+    _photoUrl = snap_data['photoURL'];
+    _gameHistory = createMatchList(snap_data['scoreHistory']);
+    _monthlyElo = createMonthlyMatchMap(snap_data['monthlyElo']);
+    _highScores = createHighScoreList(snap_data['highestScore']);
   }
 
-  User.map(dynamic obj) {
-    _userName = obj['username'];
-    _email = obj['email'];
-    _photoUrl = obj['photoURL'];
-    _gameHistory = createMatchList(obj['scoreHistory']);
-    _monthlyElo = createMonthlyMatchMap(obj['monthlyElo']);
-    _highScores = createHighScoreList(obj['highestScore']);
-  }
-
-  List<HighScore> createHighScoreList(Map<String, Object> data) {
+  List<HighScore> createHighScoreList(List<dynamic> data) {
     final List<HighScore> scores = [];
-    data.values.forEach((Object game) => scores.add(HighScore.fromJson(game)));
+    for (var score in data) {
+      final Map<String, dynamic> g = Map<String, dynamic>.from(score);
+      scores.add(HighScore.fromJson(g));
+    }
     return scores;
   }
 
-  List<Match> createMatchList(Map<String, Object> data) {
-    final List<Match> matches = [];
-    data.values.forEach((Object game) => matches.add(Match.fromJson(game)));
+  List<DartMatch> createMatchList(List<dynamic> data) {
+    final List<DartMatch> matches = [];
+    for (var game in data) {
+      final Map<String, dynamic> g = Map<String, dynamic>.from(game);
+      matches.add(DartMatch.fromJson(g));
+    }
     return matches;
   }
 
-  Map<String, List<Match>> createMonthlyMatchMap(Map<String, Map<String, Map<String, Object>>> data) {
-    final Map<String, List<Match>> matches = {};
-    data.forEach((String k, Map<String, Map<String, Object>> v) =>
-        fillMonthlyMatchMap(k, matches, v));
+  Map<String, List<DartMatch>> createMonthlyMatchMap(Map<dynamic, dynamic> data) {
+    final Map<String, List<DartMatch>> matches = {};
+
+    for (var month in data.keys) {
+      final List<dynamic> monthlyGames = data[month];
+      matches[month] = [];
+      for (var game in monthlyGames) {
+        final Map<String, dynamic> g = Map<String, dynamic>.from(game);
+        matches[month].add(DartMatch.fromJson(g));
+      }
+    }
+
+    for (List<DartMatch> gameList in matches.values) {
+      gameList.sort((DartMatch a, DartMatch b) {
+        return a.date.compareTo(b.date);
+      });
+    }
     return matches;
-  }
-
-  void fillMonthlyMatchMap(String monthKey,
-      Map<String, List<Match>> matches,
-      Map<String, Map<String, Object>> monthlyMatches) {
-
-      matches[monthKey] = createMatchList(monthlyMatches);
   }
 
   String _userName;
   String _email;
   String _photoUrl;
-  Map<String, List<Match>> _monthlyElo;
-  List<Match> _gameHistory;
+  Map<String, List<DartMatch>> _monthlyElo;
+  List<DartMatch> _gameHistory;
   List<HighScore> _highScores;
 
   String get userName => _userName;
   String get email => _email;
   String get photoUrl => _photoUrl;
-  Map<String, List<Match>> get monthlyElo => _monthlyElo;
-  List<Match> get gameHistory => _gameHistory;
+  Map<String, List<DartMatch>> get monthlyElo => _monthlyElo;
+  List<DartMatch> get gameHistory => _gameHistory;
   List<HighScore> get highScores => _highScores;
+  DartMatch get lastEloMatch => _monthlyElo['${DateTime.now().year}-${DateTime.now().month - 1}'].last;
 }
